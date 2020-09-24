a9073581ea9a18f64f4a56bbe8c3cf18c03b1684
jihwan <wngustnf@naver.com>
2020-09-24 20:51:51 +0900

    [ADD] Added a 'test'-button in NNStreamerActivity, Test the intent to new Activity

---
+++ android/example_app/nnstreamer-ssd/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o.d
@@ -0,0 +1,5 @@
+/home/ssafy/Android/workspace/nnstreamer-example/android/example_app/nnstreamer-ssd/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o: \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/android/cpufeatures/cpu-features.c \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/android/cpufeatures/cpu-features.h
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/android/cpufeatures/cpu-features.h:
---
+++ android/example_app/nnstreamer-ssd/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/nnstreamer/home/ssafy/Android/workspace/nnstreamer/gst/nnstreamer/tensor_filter/tensor_filter_support_cc.o.d
@@ -0,0 +1,149 @@
+/home/ssafy/Android/workspace/nnstreamer-example/android/example_app/nnstreamer-ssd/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/nnstreamer//home/ssafy/Android/workspace/nnstreamer/gst/nnstreamer/tensor_filter/tensor_filter_support_cc.o: \
+  /home/ssafy/Android/workspace/nnstreamer/gst/nnstreamer/tensor_filter/tensor_filter_support_cc.cc \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/string.h \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__config \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/errno.h \
+  /home/ssafy/Android/workspace/nnstreamer/gst/nnstreamer/nnstreamer_log.h \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/system_error \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__errc \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cerrno \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/type_traits \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cstddef \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/version \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stdexcept \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/exception \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cstdlib \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/iosfwd \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/wchar.h \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__functional_base \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/typeinfo \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cstdint \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stdint.h \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/new \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/utility \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__tuple \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/initializer_list \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cstring \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__debug \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/string \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/string_view \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__string \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/algorithm \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/memory \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/limits \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/iterator \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/tuple \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cassert \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/atomic \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/functional \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/bit \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cstdio \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stdio.h \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cwchar \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cwctype \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cctype \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/ctype.h \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/wctype.h \
+  /home/ssafy/Android/workspace/nnstreamer/gst/nnstreamer/include/nnstreamer_plugin_api_filter.h \
+  /home/ssafy/Android/workspace/nnstreamer/gst/nnstreamer/include/tensor_typedef.h \
+  /home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stddef.h \
+  /home/ssafy/Android/workspace/nnstreamer/gst/nnstreamer/include/nnstreamer_cppplugin_api_filter.hh
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/string.h:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__config:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/errno.h:
+
+/home/ssafy/Android/workspace/nnstreamer/gst/nnstreamer/nnstreamer_log.h:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/system_error:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__errc:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cerrno:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/type_traits:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cstddef:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/version:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stdexcept:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/exception:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cstdlib:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/iosfwd:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/wchar.h:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__functional_base:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/typeinfo:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cstdint:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stdint.h:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/new:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/utility:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__tuple:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/initializer_list:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cstring:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__debug:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/string:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/string_view:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__string:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/algorithm:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/memory:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/limits:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/iterator:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/tuple:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cassert:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/atomic:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/functional:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/bit:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cstdio:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cwchar:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cwctype:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/cctype:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/ctype.h:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/wctype.h:
+
+/home/ssafy/Android/workspace/nnstreamer/gst/nnstreamer/include/nnstreamer_plugin_api_filter.h:
+
+/home/ssafy/Android/workspace/nnstreamer/gst/nnstreamer/include/tensor_typedef.h:
+
+/home/ssafy/Android/tools/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stddef.h:
+
+/home/ssafy/Android/workspace/nnstreamer/gst/nnstreamer/include/nnstreamer_cppplugin_api_filter.hh:
