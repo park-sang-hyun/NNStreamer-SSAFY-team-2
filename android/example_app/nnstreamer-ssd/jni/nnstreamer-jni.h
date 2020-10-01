bede3152a499cf7fee162da2fc651140f6f239bc
JeongYeonUk <vire3064@gmail.com>
2020-10-02 04:17:51 +0900

    Setting Values insert map

--- android/example_app/nnstreamer-ssd/jni/nnstreamer-jni.h
+++ android/example_app/nnstreamer-ssd/jni/nnstreamer-jni.h
@@ -1,8 +1,9 @@
 /**
  * @file	nnstreamer-jni.h
- * @date	1 April 2019
+ * @date	29 September 2020
  * @brief	Common definition for tensor stream example
  * @author	Jaeyun Jung <jy1210.jung@samsung.com>
+ * @author  Yeonuk Jeong <dusdnrl1@naver.com>
  * @bug		No known bugs
  */
 
@@ -15,37 +16,43 @@
 #define TAG_NAME "NNStreamer"
 
 #define nns_logd(...) \
-    __android_log_print (ANDROID_LOG_DEBUG, TAG_NAME, __VA_ARGS__)
+    __android_log_print(ANDROID_LOG_DEBUG, TAG_NAME, __VA_ARGS__)
 
 #define nns_logi(...) \
-    __android_log_print (ANDROID_LOG_INFO, TAG_NAME, __VA_ARGS__)
+    __android_log_print(ANDROID_LOG_INFO, TAG_NAME, __VA_ARGS__)
 
 #define nns_loge(...) \
-    __android_log_print (ANDROID_LOG_ERROR, TAG_NAME, __VA_ARGS__)
+    __android_log_print(ANDROID_LOG_ERROR, TAG_NAME, __VA_ARGS__)
 
 /**
 * @brief Information for pipeline info.
 */
 typedef struct _NNSPipelineInfo
 {
-  gint id;                      /**< Id for the pipeline */
-  const gchar *name;            /**< Pipeline name */
-  const gchar *description;     /**< Pipeline description */
+    gint id;                  /**< Id for the pipeline */
+    const gchar *name;        /**< Pipeline name */
+    const gchar *description; /**< Pipeline description */
 
-  gboolean (*init) (void);
-  void (*free) (void);
-  gboolean (*get_name) (gchar **name, const gint option);
-  gboolean (*get_description) (gchar **desc, const gint option);
-  gboolean (*prepare_pipeline) (const gint option);
-  gboolean (*launch_pipeline) (GstElement **pipeline, const gint option);
+    gboolean (*init)(void);
+    void (*free)(void);
+    gboolean (*get_name)(gchar **name, const gint option);
+    gboolean (*get_description)(gchar **desc, const gint option);
+    gboolean (*prepare_pipeline)(const gint option);
+    gboolean (*launch_pipeline)(GstElement **pipeline, const gint option);
 } NNSPipelineInfo;
+
+typedef struct _SettingData
+{
+    gchar name[256];
+    gint count;
+}SettingData;
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-gboolean nns_register_pipeline(NNSPipelineInfo *info);
+    gboolean nns_register_pipeline(NNSPipelineInfo *info);
 
 #ifdef __cplusplus
 }
